82d8ddaa9dc241c5daaab33fad00e5e9b171004b
Ivana-Antonella <antonellacarmona090@gmail.com>
2023-08-02 22:54:52 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from "./redux/store/store";
+import axios from 'axios';
 // import './index.css';
 import App from './App';
-// axios.defaults.baseURL = 'http://localhost:3001'
+
+
+
 axios.defaults.baseURL = 'https://backendvideogames.up.railway.app'
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <Provider store={store}>
