bdbd2f6a72b54419cdaf90a581eb12ea8493559f
Ivana-Antonella <antonellacarmona090@gmail.com>
2023-09-11 15:19:33 -0300

    Working

--- src/views/Form/Form.jsx
+++ src/views/Form/Form.jsx
@@ -1,250 +1,3 @@
-// import { useState, useEffect } from "react";
-// import style from "./Form.module.css";
-// import {getAllGenres, getAllPlatforms, postGames, getAllGames} from "../../redux/actions/actions";
-// import {useDispatch, useSelector} from "react-redux";
-// import { useHistory } from 'react-router-dom';
-// import { Link } from "react-router-dom/cjs/react-router-dom.min";
-// import {uploadFile} from "../../firebase/config"
-
-
-
-
-// const Form = () =>{
-//   const dispatch = useDispatch();
-//   // const navigate= useNavigate()
-//   const genres = useSelector((state) => state.genres);
-//   const platforms = useSelector((state)=> state.platforms);
-//   const history = useHistory();
-
-//   //manejador inputs
-//   const [input, setInput] = useState({
-//     name:"",   
-//     description:"",
-//     released:"",
-//     rating:"",
-//     image:"",
-//     platforms: [],
-//     genres: []
-// });
-
-// //manejador state de errores______________________________________________
-// const [error, setError] = useState({
-//     name:"",   
-//     description:"",
-//     released:"",
-//     rating:"",
-//     image:"",
-//     platforms: [],
-//     genres: []
-// });
-// //__________________________________________________________________________
-
-// const [file, setFile] = useState(null)
-
-// //_________________________________________________________________________ 
-
-//   // esta fn maneja el state local-> controladorCambios de los input_____________
-//   // //tiene que leer lo que escribi, y guardarlo en el estado local_____________
-//   const changeHandler = (event) =>{
-//     const propiedad= event.target.name;
-//     const valor = event.target.value;
-  
-
-//    setInput({...input, [propiedad]:valor})
-//    setError(validate({...input, [propiedad]:valor})) 
-    
-//   }
-
-
-//  // fn valida que lo que se escriba en los input sea correcto_____________________________
-//  const validate = (input) =>{
-//    let errorA={};
-
-//       if (/^[a-zA-ZÀ-ÖØ-öø-ÿ\s']+$/.test(input.name)) {
-//     errorA.name = "✔️";
-//   } else if (input.name === "" || input.name[0].trim().length === 0) {
-//     errorA.name = "Se debe completar este campo";
-//   } else {
-//     errorA.name = "Solo permite letras del alfabeto (a-z, A-Z)";
-//   }
-   
-//    //______________________________________________________________________________________
-//    if(input.description.length>0 && input.description.length > 20 && input.description.length < 200){
-//     errorA.description= "✔️"
-//    } else if(input.description==="" || input.description[0].trim().length === 0) {
-//     errorA.description= "Se debe completar este campo"}
-//    //______________________________________________________________________________________
-//     if (file) {
-//       errorA.image = "✔️";
-//     } else {
-//       errorA.image = "Se debe completar este campo";
-//     }
-//    //__________________________________________________________________________________
-//    if(input.released){
-//     errorA.released="✔️";
-//    } else if(input.released === ""){
-//     errorA.released="Se debe completar este campo";
-//    }
-//    //_________________________________________________________________________________
-//    if(!input.rating){
-//     errorA.rating= "El rating es obligatorio";
-//    } 
-//    else if(input.rating > 5 || input.rating < 0) {
-//       errorA.rating="El rating debe estar entre 0 y 5";
-//    }
-//    else{
-//     errorA.rating= "✔️"
-//    }
-//    //_________________________________________________________________________________
-//    if(!input.genres){
-//    errorA.genres= "El genero es obligatorio";
-//    } 
-//    else {
-//     errorA.genres= "✔️"
-//    }
-// //____________________________________________________________________________________
-//    if(!input.platforms){
-//     errorA.platforms= "Seleccione una plataforma";
-//     } 
-//     else {
-//      errorA.platforms= "✔️"
-//     }
-
-//    return errorA;
-//  } 
-
-
-//  //_________________________FN BOTON CREAR__________________________________________________________
-//  const submitHandler = async (event)=>{
-//     event.preventDefault();
-      
-//     if(input && file){
-      
-//         try {
-//           const imageUrl = await uploadFile(file); // Subir la imagen a Firebase Storage
-//           setInput({ ...input, image: imageUrl }); // Actualizar la URL de la imagen en el estado
-//           dispatch(postGames({ ...input, image: imageUrl }))
-//         } catch (error) {
-//           console.error("Error al subir la imagen:", error);
-//           return
-//         }
-      
-//       // dispatch(postGames(input))
-    
-     
-//     alert(`Has creado el juego ${input.name} exitosamente`)
-//     setInput({
-//       name: "",
-//       description: "",
-//       released: "",
-//       image: "",
-//       platforms: [],
-//       genres: [],
-//       rating: ""
-//      });
-//     history.push("/home")
-
-//   }else{
-//     alert("falta algunos campos")
-//   }
- 
-//  }
-// //______________________________________________________________________________
-
-
-//  useEffect(() => {
-//   dispatch(getAllGenres()); // Despacha la acción para obtener los géneros
-// }, [dispatch]);
-
-// useEffect(() => {
-//   dispatch(getAllPlatforms()); // Despacha la acción para obtener las platforms
-// }, [dispatch]);
-
-// //_______________________________________________________________________________________
-//     return(
-//     <div className={style.containerFormulario}>
-
-//        <Link to="/home">
-//           <div >
-//             <button id="work" type="button" name="Hover" className={style.ButtonForm}>
-//               Volver
-//             </button>
-//           </div>
-//         </Link>
-
-//      <form onSubmit={submitHandler} className={style.formulario}>
-//         <div className={style.formColumns}>    
-
-//             <div className={style.formColumn}>
-//               <label>Name</label>
-//               <input type="text" value={input.name} onChange={changeHandler} name="name" required/>
-//               <span className={style.error}>{error.name && error.name}</span>
-//             </div>
-
-//               <div className={style.formColumn}>
-//               <label>Fecha de lanzamiento</label>
-//               <input type="date" value={input.released} onChange={changeHandler} name="released" required/>
-//               {error.released && <span  className={style.error}>{error.released}</span>}
-//               </div>
-
-//               <div className={style.formColumn}>
-//               <label>Rating</label>
-//               <input type="number" value={input.rating} onChange={changeHandler} name="rating" required/>
-//               {error.rating && <span  className={style.error}>{error.rating}</span>}
-//               </div>
-
-//               <div className={style.formColumn}>
-//               <label>Platforms</label>
-//               <select name="platforms" onChange={changeHandler} required  value={input.platforms}>
-//               <option value="">Elegir</option>
-//                     {platforms && platforms.map((name) => (
-//                        <option key={name} value={name}>
-//                           {name}
-//                        </option> ))}
-//               </select>
-//               {/* {error.platforms && <span>{error.platforms}</span>} */}
-//               </div>
-
-//               <div className={style.formColumn} >
-//               <label>Genero</label>
-//               <select onChange={changeHandler} required name="genres"  value={input.genres} >
-//               <option value="">Elegir</option>
-//                  {genres && genres.map((genre) => (
-//                     <option key={genre.id} value={genre.name}>
-//                       {genre.name}
-//                     </option>
-//                   ))}
-//               </select>
-//               {error.genres && <span>{error.genres}</span>}
-//              </div>
-
-//              <div className={style.formColumn}>  
-//               <label>Imagen</label>
-//               <input type="file" value={input.image.event} onChange={evento => setFile(evento.target.files[0])} name="image" required />
-//               {file && <span className={style.fileName}>{file.name}</span>}
-//               {error.image && <span  className={style.error}>{error.image}</span>}
-//             </div> 
-            
-//             <div className={style.formColumn}>
-//               <label>Description</label>
-//               <textarea type="text" value={input.description} onChange={changeHandler} name="description" required rows="5" cols="39"/>
-//               {error.description && <span  className={style.error}>{error.description}</span>}
-//               </div>
-
-//               <button type="submit"   className={style.submitButton}>CREAR</button>
-//             </div>
-//           </form>
-//       </div>
-//     )
-//   } 
-  
-//   export default Form;
-
-
-
-
-// ---------------------------------------------------------------------------------
-
 import { useState, useEffect } from "react";
 import style from "./Form.module.css";
 import {getAllGenres, getAllPlatforms, postGames} from "../../redux/actions/actions";
@@ -292,7 +45,7 @@ const [modalIsOpen, setModalIsOpen] = useState(false);
 //_________________________________________________________________________ 
 
   // esta fn maneja el state local-> controladorCambios de los input_____________
-  // //tiene que leer lo que escribi, y guardarlo en el estado local_____________
+ //lee el contenido del input, y guarda en el estado local_____________
   const changeHandler = (event) =>{
     const propiedad= event.target.name;
     const valor = event.target.value;
@@ -355,42 +108,6 @@ const [modalIsOpen, setModalIsOpen] = useState(false);
 
 
  //_________________________FN BOTON CREAR__________________________________________________________
-//  const submitHandler = async (event)=>{
-//     event.preventDefault();
-      
-//     if(input && file){
-      
-//         try {
-//           const imageUrl = await uploadFile(file); // Subir la imagen a Firebase Storage
-//           setInput({ ...input, image: imageUrl }); // Actualizar la URL de la imagen en el estado
-//           dispatch(postGames({ ...input, image: imageUrl }))
-//         } catch (error) {
-//           console.error("Error al subir la imagen:", error);
-//           return
-//         }
-    
-     
-//     alert(`Has creado el juego ${input.name} exitosamente`)
-//     setInput({
-//       name: "",
-//       description: "",
-//       released: "",
-//       image: "",
-//       platforms: [],
-//       genres: [],
-//       rating: ""
-//      });
-//     history.push("/home")
-
-//   }else{
-//     alert("falta algunos campos")
-//   }
- 
-//  }
-
-
-
-
 const submitHandler = async (event) => {
   event.preventDefault();
 
@@ -418,13 +135,6 @@ const closeModal = () => {
 
 
 //___________________BOTON SIGUIENTE FORMULARIO________________________________
-  // const nextFormHandler= ()=>{
-  //  // Verifica si el usuario está en la última página (en este caso, la página 4)
-  // if (currentPage < 4 && input.name && input.released) {
-  //   setCurrentPage(currentPage + 1);
-  // } 
-  // }
-
   const nextFormHandler = () => {
     // Verifica si el usuario está en la última página (en este caso, la página 4)
     if (currentPage < 4) {
@@ -635,10 +345,10 @@ useEffect(() => {
             backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fondo oscuro tras el modal
           },
           content: {
-            width: '40%',  // Ancho del modal
+            width: 'auto',  // Ancho del modal
             maxWidth: '300px', // Ancho máximo del modal
-            maxHeight: "300",
-            height:"38%",
+            maxHeight: "300px",
+            height:"auto",
             margin: 'auto', // Centrar el modal horizontalmente
             padding: '20px', // Espaciado interno del contenido del modal
             borderRadius: '8px', // Bordes redondeados
@@ -663,19 +373,3 @@ useEffect(() => {
   
   export default Form;
 
-
-
-
-
-
-  
-       /* <Link to="/home">
-          <div >
-            <button id="work" type="button" name="Hover" className={style.ButtonForm}>
-              Volver
-            </button>
-          </div>
-        </Link> */
-
-
-        //  
